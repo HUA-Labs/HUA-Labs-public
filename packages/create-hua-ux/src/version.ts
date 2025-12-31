3db224889c1c7f04a2ef555227e94aa6c859e320
gr22nist <gr22nist@gmail.com>
2025-12-31 22:22:51 +0900

    fix: (create-hua-ux) resolve installation failure and upgrade node requirement to 22.x

--- packages/create-hua-ux/src/version.ts
+++ packages/create-hua-ux/src/version.ts
@@ -2,8 +2,8 @@
  * This file is auto-generated at build time.
  * Do not edit manually.
  * 
- * Generated from: C:\HUA-Labs-public\packages\hua-ux\package.json
- * Source version: 0.1.0-alpha.0.2
+ * Generated from: D:\HUA\hua-labs-public\packages\hua-ux\package.json
+ * Source version: 0.1.0-alpha.1
  */
 
-export const HUA_UX_VERSION = '^0.1.0-alpha.0.2';
+export const HUA_UX_VERSION = '^0.1.0-alpha.1';
